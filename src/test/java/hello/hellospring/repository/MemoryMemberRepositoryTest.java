d602558ed40d66317db47ae37f3dbdc0fd270010
hyena0608 <goatsfish@likelion.org>
2021-07-13 23:07:26 +0900

    spring boot

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+
+    }
+
+}
